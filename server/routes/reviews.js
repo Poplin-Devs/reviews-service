59be251871a82f8baa4ffe942ba1cda45de6ef99
Andrew Carnero <andrew.carnero@gmail.com>
2022-01-18 14:21:34 -0500

    made progress on /reviews query, pausing to build out other routes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -17,8 +17,39 @@ module.exports = {
     console.log('\nrequest parameters: ', req.query);
     console.log('page, sort, count: ', page, sort, count, '\n');
 
-    const sql = `SELECT * FROM reviews JOIN photos ON photos.review_id = reviews.review_id
-      WHERE product_id = ${product_id} AND reported = (0) ORDER BY ${sortColumn} LIMIT ${count}`;
+    // const sql = `SELECT review_id, product_id, rating, summary, recommend, body, review_date, helpfulness, response, name
+    //   FROM reviews JOIN users ON users.user_id = reviews.reviewer_id
+    //   WHERE product_id = ${product_id} AND reported = (0) ORDER BY ${sortColumn} LIMIT ${count}`;
+
+    // const sql2 = `
+    //   SELECT id, url FROM reviews
+    //   INNER JOIN photos
+    //   ON photos.review_id = reviews.review_id
+    //   WHERE reviews.review_id = 5`;
+
+
+    const sql = `
+    SELECT review_id, product_id, rating, summary, recommend, body, review_date, helpfulness, response, name,
+    FROM reviews JOIN users ON users.user_id = reviews.reviewer_id
+    WHERE product_id = ${product_id} AND reported = (0) ORDER BY ${sortColumn} LIMIT ${count}`;
+
+    // const sql2 = `
+    //   SELECT id, url FROM reviews
+    //   INNER JOIN photos
+    //   ON photos.review_id = reviews.review_id
+    //   WHERE reviews.review_id = 5`;
+
+    // db.connection.query(sql, (error, results) => {
+    //   if (error) {
+    //     res.status(400).send(error);
+    //   }
+
+    //   console.log(results);
+
+    //   res.status(200).json(results);
+
+    // });
+
 
     db.connection.query(sql, (error, results) => {
       if (error) {
@@ -28,7 +59,7 @@ module.exports = {
       console.log(results);
 
       const reviews = results.map((result) => {
-        const { review_id, rating, summary, response, body, review_date, helpfulness, reviewer_id } = result;
+        const { review_id, rating, summary, response, body, review_date, helpfulness, name } = result;
         let { recommend } = result;
         recommend.toJSON().data[0] === 0 ? recommend = false : recommend = true;
 
@@ -40,7 +71,7 @@ module.exports = {
           response: response ? response : '',
           body: body,
           date: review_date,
-          //reviewer_name: TODO,
+          reviewer_name: name,
           helpfulness: helpfulness,
           //photos: TODO
         };
